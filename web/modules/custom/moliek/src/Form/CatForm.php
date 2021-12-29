9d51096d246cd381a6e40f5a8a6ba5897f291605
Maks <maksym.oleksyuk@gmail.com>
2021-12-29 17:31:35 +0200

    use ajax in validate form

--- web/modules/custom/moliek/src/Form/CatForm.php
+++ web/modules/custom/moliek/src/Form/CatForm.php
@@ -4,6 +4,9 @@ namespace Drupal\moliek\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\MessageCommand;
+use Drupal\Core\Messenger\Messenger;
 
 /**
  * Implements an example form.
@@ -27,13 +30,18 @@ class CatForm extends FormBase {
       '#type' => 'textfield',
       '#title_display' => 'before',
       '#title' => $this->t('Your cat’s name:'),
-      '#pattern' => '^(?!\s*$)[0-9a-zA-Zа-яА-ЯіІїЇЁё`\' ]{2,32}$',
       '#placeholder' => $this->t("should be in the range of 2 and 32 symbols"),
     ];
     $form['submit'] = [
       '#type' => 'submit',
-      '#button_type' => 'primary',
       '#value' => $this->t("Add cat"),
+      '#ajax' => [
+        'event' => 'click',
+        'callback' => '::myAjax',
+        'progress' => [
+          'message' => NULL,
+        ],
+      ],
     ];
     return $form;
   }
@@ -51,10 +59,27 @@ class CatForm extends FormBase {
   }
 
   /**
+   * Setting the message in our form.
+   */
+  public function myAjax(array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    if ($form_state->getErrors()) {
+      foreach ($form_state->getErrors() as $err) {
+        $response->addCommand(new MessageCommand($err, NULL, ['type' => 'error']));
+      }
+      $form_state->clearErrors();
+    }
+    else {
+      $response->addCommand(new MessageCommand(t('Your cat added successfully.')));
+    }
+    $this->messenger()->deleteAll();
+    return $response;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus($this->t('Сat added successfully'));
   }
 
 }
